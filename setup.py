11f3e86c11231d41df726bcea0fb68380371d286
gissehel <public-devgit-cochir@gissehel.org>
2013-04-19 23:08:24 +0200

    The dependancy is to mega.py not megapy

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='megacl',
-      version='0.1.1',
+      version='0.1.2',
       description='mega.co.nz command line client.',
       author='Arthibus Gisséhel',
       author_email='public-dev-megacl@gissehel.org',
@@ -14,7 +14,7 @@ setup(name='megacl',
       license='MIT',
       keywords='commandline mega.co.nz mega',
       long_description=open('README.md').read(),
-      install_requires=['supertools','cltools','megapy'],
+      install_requires=['supertools','cltools','mega.py'],
       classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
