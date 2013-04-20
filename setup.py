e5da5b3b3a3a9b31ac6e2b87d3be6d90875d9365
gissehel <public-devgit-cochir@gissehel.org>
2013-04-20 16:30:33 +0200

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='megacl',
-      version='0.1.4',
+      version='0.2.0',
       description='mega.co.nz command line client.',
       author='Arthibus Gisséhel',
       author_email='public-dev-megacl@gissehel.org',
