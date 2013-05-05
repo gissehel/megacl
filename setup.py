aac8d59ed95d73065bf3452d51ae166050b6031d
gissehel <public-devgit-cochir@gissehel.org>
2013-05-05 14:36:38 +0200

    Version for testspeed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='megacl',
-      version='0.4.3-testspeed',
+      version='0.4.3-testspeed-2',
       description='mega.co.nz command line client.',
       author='Arthibus Gisséhel',
       author_email='public-dev-megacl@gissehel.org',
